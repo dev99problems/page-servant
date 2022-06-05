22ee66f60868ba9f485619447137bfe9ed3fd840
Eugene Chulkov <genechulkov@gmail.com>
2022-06-05 16:46:54 +0300

    update: make page addition work

--- packages/page-add/src/index.js
+++ packages/page-add/src/index.js
@@ -1,11 +1,32 @@
-import { main as page_servant_main, get_notion } from '@dev99problems/page-servant_notion';
+import { main as createNewPage, get_notion as getNotion } from '@dev99problems/page-servant_notion';
 
-const notion = get_notion(NOTION_TOKEN);
+// NOTE: NOTION_TOKEN & JOURNAL_ID are
+// worker vars, set through the dashboard
+const TOPIC = 'ruby';
+const notion = getNotion(NOTION_TOKEN);
 
-addEventListener('scheduled', event => {
-  return event.waitUntil(triggerEvent(event.scheduledTime));
+addEventListener('scheduled', (event) => {
+  event.waitUntil(triggerEvent(event.scheduledTime));
 });
 
-async function triggerEvent() {
-  await page_servant_main(notion, JOURNAL_ID, TOPIC);
+async function triggerEvent(timestamp) {
+  console.log('timestamp of last trigger', timestamp);
+  await createNewPage(notion, JOURNAL_ID, TOPIC);
 }
+
+
+// NOTE: this is for local testing
+/*addEventListener('fetch', (event) => {
+  return event.respondWith(handleFetch(event.request));
+});
+
+async function handleFetch (req) {
+  const url = new URL(req?.url);
+  const {pathname} = url;
+
+  if (pathname !== '/favicon.ico') {
+    await triggerEvent();
+  }
+
+  return new Response('Hello worker!', {status: 200});
+};*/
